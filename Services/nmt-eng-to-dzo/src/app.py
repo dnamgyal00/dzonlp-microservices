42480d002f89fbdb2f6550f064787868fde659bc
Kabita Ghalley <divyashchhetri@gmail.com>
2024-06-15 14:48:40 +0600

    Added dict.json

--- Services/nmt-eng-to-dzo/src/app.py
+++ Services/nmt-eng-to-dzo/src/app.py
@@ -3,21 +3,29 @@ from flask_cors import CORS
 import logging
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 from flask import Flask, request, jsonify
+import json
 
 app = Flask(__name__)
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
-model_path = r"D:\dzonlp-microservices\Services\nmt-eng-to-dzo\model\en_to_dz"  # Use raw string (r"") to avoid escape characters
-tokenizer_path = r"D:\dzonlp-microservices\Services\nmt-eng-to-dzo\model\tokenizer_en_to_dz"  # Use raw string (r"") to avoid escape characters
+model_path = "../model/en_to_dz"  # Use raw string (r"") to avoid escape characters
+tokenizer_path = "../model/tokenizer_en_to_dz"  # Use raw string (r"") to avoid escape characters
 
 model = AutoModelForSeq2SeqLM.from_pretrained(model_path)
 tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
 
+with open('../../dict.json', 'r', encoding='utf-8') as f:
+    translation_dict = json.load(f)
+
 def translate(text):
     try:
-        # Translate the text.
+        # Check if text is a single word and exists in the dictionary
+        if text.lower() in translation_dict:
+            return translation_dict[text.lower()]
+
+        # If not in the dictionary, use the model
         encoded_inputs = tokenizer(text, return_tensors="pt")
         outputs = model.generate(**encoded_inputs)
         translated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
